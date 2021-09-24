ad7a9dc507bd0b945eddff010cc40ca9318ac09d
Fikcup <rhys.n.wright@gmail.com>
2021-09-24 09:36:21 -0700

    feat : added contact form shell

---
+++ src/components/Contact/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// TODO: Finish contact me form JSX
+// TODO: Add form field validation
+
+const Contact = () => {
+    return (
+        <form>
+            <div>
+                <label></label>
+                <input />
+            </div>
+        </form>
+    );
+}
+
+export default Contact;
