bb66acb1c4d5e8febe8eda28210c074ada83b216
Fikcup <rhys.n.wright@gmail.com>
2021-09-24 09:36:03 -0700

    feat : added resume component

---
+++ src/components/Resume/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Resume = () => {
+    return (
+        <section>
+            <a href="../public/assets/Resume.pdf" download>Resume</a>
+
+            <h2>Languages</h2>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Express.js</li>
+                <li>Node.js</li>
+                <li>SQL</li>
+                <li>Mongoose</li>
+            </ul>
+
+            <h2>Technologies</h2>
+            <ul>
+                <li></li>
+                <li>Insomnia</li>
+                <li>MySQL</li>
+                <li>MongoDB</li>
+                <li>MariaDB</li>
+            </ul>
+        </section>
+    )
+}
+
+export default Resume;
