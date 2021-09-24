40d94c882fd4114ea9e6f78655e1a49944c8416c
Fikcup <rhys.n.wright@gmail.com>
2021-09-24 09:38:19 -0700

    feat : added portfolio list shell

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Project from '../Project';
+
+
+// TODO: Create JSON file with props and map to create a list of projects
+
+const Portfolio = () => {
+    render (
+        <Project />
+    );
+}
+
+export default Portfolio;
