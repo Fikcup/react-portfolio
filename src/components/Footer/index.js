0bd0200a7189bb3f94731c431393f3f246bbe084
Fikcup <rhys.n.wright@gmail.com>
2021-09-24 09:36:42 -0700

    feat : added footer component shell

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// TODO: Add links
+
+const Footer = () => {
+    return (
+        <footer>
+            <ul>
+                <li><a>GitHub</a></li>
+                <li><a>LinkedIn</a></li>
+                <li><a>Email</a></li>
+            </ul>
+        </footer>
+    );
+}
+
+export default Footer;
