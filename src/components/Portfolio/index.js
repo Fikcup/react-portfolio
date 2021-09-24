f2d837eca26c3ec194941a39680d9173147e5128
Fikcup <rhys.n.wright@gmail.com>
2021-09-24 13:06:48 -0700

    feat : project props read from JSON and mapped into portfolio component

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { render } from 'react-dom';
 import Project from '../Project';
+import projects from './projects.json';
 
 
 // TODO: Create JSON file with props and map to create a list of projects
 
 const Portfolio = () => {
-    render (
-        <Project />
+    return (
+        <section>
+            {projects.map((projects) => (
+                <Project 
+                    name = {projects.name}
+                    src = {projects.src}
+                    deployment = {projects.deployment}
+                    github = {projects.github}
+                    desc = {projects.desc}
+                />
+            ))}
+        </section>
     );
 }
 
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import { render } from 'react-dom';
 
 // TODO: Create JSON file that implements these props
 
 const Project = (props) => {
-    render (
+    return (
         <div>
-            <a target="_blank" href={props.deployment}>
+            <a target="_blank" rel="noreferrer" href={props.deployment}>
                 <img src={props.src} alt={props.name} />
                 <h2>{props.name}</h2>
             </a>
             <h3>
-                <a target="_blank" href={props.github}>
+                <a target="_blank" rel="noreferrer" href={props.github}>
                     Code here
                 </a>
             </h3>
+            <p>{props.desc}</p>
         </div>
     );
 }
