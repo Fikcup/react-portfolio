441e2ebc3035131f1ac5f146ebae5a1ac704ab7c
Fikcup <rhys.n.wright@gmail.com>
2021-09-24 19:26:42 -0700

    fix : fixed profile picture link

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <section>
-            <img src="../public/assets/profile-picture" alt="Rhys Wright" />
+            <img src="../assets/profile-picture.jpg" alt="Rhys Wright" />
 
             <p>Rhys Wright is a software developer focused on improving and gaining additional experience in the tech industry. They primarily develop web applications using HTML, CSS, JavaScript, React, and SQL. Rhys is a self taught developer who currently works as a full stack web development tutor and centralized grader for 2U. They are currently looking to gain additional experience working with the full software development life cycle.</p>
         </section>
