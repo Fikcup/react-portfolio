d2a89f5ce94b683975f9d7c9737fef5b30dcd7b5
Fikcup <rhys.n.wright@gmail.com>
2021-09-24 20:30:44 -0700

    feat : added a list of proficiencies

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -5,25 +5,32 @@ const Resume = () => {
         <section>
             <a href="../public/assets/Resume.pdf" download>Resume</a>
 
-            <h2>Languages</h2>
+            <h2>Front-End Languages</h2>
             <ul>
                 <li>HTML</li>
                 <li>CSS</li>
+                <li>SASS</li>
+                <li>Tailwind</li>
+                <li>Bootstrap</li>
                 <li>JavaScript</li>
                 <li>React</li>
+                <li>Redux</li>
+            </ul>
+
+            <h2>Back-End Languages</h2>
+            <ul>
                 <li>Express.js</li>
                 <li>Node.js</li>
                 <li>SQL</li>
                 <li>Mongoose</li>
             </ul>
 
-            <h2>Technologies</h2>
+            <h2>APIs</h2>
             <ul>
-                <li></li>
-                <li>Insomnia</li>
-                <li>MySQL</li>
-                <li>MongoDB</li>
-                <li>MariaDB</li>
+                <li>REST</li>
+                <li>GQL</li>
+                <li>Apollo Server</li>
+                <li>Stripe</li>
             </ul>
         </section>
     )
