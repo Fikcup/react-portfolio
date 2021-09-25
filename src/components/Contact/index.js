8b7b1cebedd6a6a9af0f036e9d0a488abd1838b6
Fikcup <rhys.n.wright@gmail.com>
2021-09-24 20:24:36 -0700

    feat : added contact me form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,15 +1,37 @@
 import React from 'react';
 
-// TODO: Finish contact me form JSX
-// TODO: Add form field validation
+// TODO: Import form field validation from utils/helpers.js
 
 const Contact = () => {
     return (
-        <form>
-            <div>
-                <label></label>
-                <input />
-            </div>
+        <form action="mailto:rhys.n.wright@gmail.com?subject=Work Opportunity" 
+        method="POST" 
+        encType="multipart/form-data"
+        name="EmailForm">
+            <label htmlFor="name">Name:</label><br />
+            <input
+                type="text"
+                name="name"
+                defaultValue="name"
+            /><br />
+            <label htmlFor="email">Email Address:</label><br />
+            <input 
+                type="email"
+                name="email"
+                defaultValue="email"
+            /><br />
+            <label htmlFor="message">Message:</label><br />
+            <textarea 
+                name="message"
+                rows="3"
+                defaultValue="message"
+            /><br />
+            <input
+                type="submit"
+                name="submit"
+                id="submit"
+                value="Submit"
+            />
         </form>
     );
 }
