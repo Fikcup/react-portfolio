91a8b331696fcb6bf2a36aa4f9658f7fc9bc7e09
Fikcup <rhys.n.wright@gmail.com>
2021-10-01 17:23:29 -0700

    feat : footer links now open in a new tab

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,9 +5,9 @@ const Footer = () => {
     return (
         <footer>
             <ul>
-                <li><a href="https://github.com/Fikcup">GitHub</a></li>
-                <li><a href="https://www.linkedin.com/in/rhys-wright/">LinkedIn</a></li>
-                <li><a href="mailto:rhys.n.wright@gmail.com">Email</a></li>
+                <li><a target="_blank" rel="noreferrer" href="https://github.com/Fikcup">GitHub</a></li>
+                <li><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/rhys-wright/">LinkedIn</a></li>
+                <li><a  href="mailto:rhys.n.wright@gmail.com?subject=Mail From Portfolio">Email</a></li>
             </ul>
         </footer>
     );
