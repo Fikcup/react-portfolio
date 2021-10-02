47dd828e14b6ca3b0b4ebac69f9891785dcb7365
Fikcup <rhys.n.wright@gmail.com>
2021-10-01 17:21:29 -0700

    refactor : improved about me paragraph

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,7 +8,11 @@ const About = () => {
 
             <img src="../assets/profile-picture.jpg" alt="Rhys Wright" id="profileImg"/>
 
-            <p>Rhys Wright is a software developer focused on improving and gaining additional experience in the tech industry. They primarily develop web applications using HTML, CSS, JavaScript, React, and SQL. Rhys is a self taught developer who currently works as a full stack web development tutor and centralized grader for 2U. They are currently looking to gain additional experience working with the full software development life cycle.</p>
+            <p>Rhys Wright is a software developer focused on improving and gaining additional experience in the tech industry. They primarily develop fullstack web applications using a variety of languages and APIs.<br /><br />
+            
+            Rhys is a self taught developer who currently works as a full stack web development tutor and centralized grader for 2U. They also work as a Teaching Assistant for 2U cohorts across a variety of colleges and universities.<br /><br />
+            
+            They are currently seeking opportunities to gain additional experience working with the full software development life cycle and a development team.</p>
         </section>
     );
 }
