5681c4d39943ae036160dceb2f7e0d88ef1e677c
Fikcup <rhys.n.wright@gmail.com>
2021-10-02 18:31:24 -0700

    feat : added more containers to resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,40 +1,69 @@
 import React from 'react';
 import './style.scss';
+import { MDFileDownload } from './react-icons/md';
 
 const Resume = () => {
     return (
         <section id="resume">
             <h2 className="sectionTitle">Resume</h2>
 
-            <a href="../public/assets/Resume.pdf" download>Resume</a>
+            <a href="../public/assets/Resume.pdf" download target="_blank" rel="noreferrer" id="download">
+                <span id="downloadIcon"><MdFileDownload size={25}/></span>
+                Download Resume as a PDF
+            </a>
 
-            <h2>Front-End Languages</h2>
-            <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>SASS</li>
-                <li>Tailwind</li>
-                <li>Bootstrap</li>
-                <li>JavaScript</li>
-                <li>React</li>
-                <li>Redux</li>
-            </ul>
+            <article id="languagesContainer">
+                <div className="languageList">
+                    <h3 className="languageSection">Languages</h3>
+                    <ul>
+                        <li>JavaScript</li>
+                        <li>C#</li>
+                        <li>Java</li>
+                    </ul>
+                </div>
 
-            <h2>Back-End Languages</h2>
-            <ul>
-                <li>Express.js</li>
-                <li>Node.js</li>
-                <li>SQL</li>
-                <li>Mongoose</li>
-            </ul>
+                <div className="languageList">
+                    <h3 className="languageSection">Front-End</h3>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>SASS</li>
+                        <li>Tailwind</li>
+                        <li>Bootstrap</li>
+                        <li>React</li>
+                        <li>Redux</li>
+                    </ul>
+                </div>
 
-            <h2>APIs</h2>
-            <ul>
-                <li>REST</li>
-                <li>GQL</li>
-                <li>Apollo Server</li>
-                <li>Stripe</li>
-            </ul>
+                <div className="languageList">
+                    <h3 className="languageSection">Back-End</h3>
+                    <ul>
+                        <li>Express.js</li>
+                        <li>Node.js</li>
+                        <li>JSON</li>
+                        <li>REST</li>
+                        <li>GraphQL</li>
+                    </ul>
+                </div>
+
+                <div className="languageList">
+                    <h3 className="languageSection">Databases</h3>
+                    <ul>
+                        <li>MongoDB</li>
+                        <li>MySQL</li>
+                        <li>MariaDB</li>
+                    </ul>
+                </div>
+
+                <div className="languageList">
+                    <h3 className="languageSection">Software</h3>
+                    <ul>
+                        <li>Unity</li>
+                        <li>Insomnia</li>
+                        <li>Figma</li>
+                    </ul>
+                </div>
+            </article>
         </section>
     )
 }
