06e7455c5bbc7fbe751f21c6f1296ae9808f237b
Fikcup <rhys.n.wright@gmail.com>
2021-10-05 15:06:00 -0700

    refactor : changed to hashrouter in app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.scss';
 import Header from './components/Header'
 import Nav from './components/Nav';
