55dc75ad07ba7ffa7fa763fd34c2931ef9952581
Fikcup <rhys.n.wright@gmail.com>
2021-10-05 13:41:50 -0700

    feat : added additional contact information

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,10 @@
 import React, { useRef } from 'react';
 import emailjs from 'emailjs-com';
 import './style.scss';
+import Icon from '../Icon';
+import { HiOutlineMail } from 'react-icons/hi';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { AiOutlineGithub } from 'react-icons/ai';
 
 export const Contact = () => {
     const form = useRef();
@@ -19,16 +23,34 @@ export const Contact = () => {
     return (
         <section id="contact">
             <h2 className="sectionTitle">Let's Work Together</h2>
-             
-            <form ref={form} onSubmit={sendEmail} name="contactForm">
-                <label>Name</label><br />
-                <input type="text" name="user_name" /><br />
-                <label>Email</label><br />
-                <input type="email" name="user_email" /><br />
-                <label>Message</label><br />
-                <textarea name="message" /><br />
-                <input type="submit" value="Send" />
-            </form>
+            
+            <article className="emailForm contactContainer">
+                <h3 className="contactTitle">Send me an email</h3>
+                <form ref={form} onSubmit={sendEmail} name="contactForm">
+                    <label>Name</label><br />
+                    <input type="text" name="user_name" /><br />
+                    <label>Email</label><br />
+                    <input type="email" name="user_email" /><br />
+                    <label>Message</label><br />
+                    <textarea name="message" /><br />
+                    <input type="submit" value="Send" id="submit"/>
+                </form>
+            </article>
+
+            <article className="contactContainer" id="contactInfo">
+                <h3 className="contactTitle">Or contact me here</h3>
+                <ul className="contactList">
+                    <Icon link="https://github.com/fikcup"
+                    text="github.com/fikcup"
+                    ><AiOutlineGithub size={40} /></Icon>
+                    <Icon link="https://www.linkedin.com/in/rhys-wright/"
+                    text="linkedin.com/rhys-wright"
+                    ><FaLinkedinIn size={40} /></Icon>
+                    <Icon link="mailto:rhys.n.wright@gmail.com?subject=Mail from Portfolio"
+                    text="rhys.n.wright@gmail.com"
+                    ><HiOutlineMail size={40} /></Icon>
+                </ul>
+            </article>
         </section>
     );
 }
