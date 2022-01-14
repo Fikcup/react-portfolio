a9d86cae0a74cd0ce281c31b10d42ee3412a0d1e
Fikcup <rhys.n.wright@gmail.com>
2022-01-13 18:08:40 -0800

    docs : updated about me statement

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,11 +8,11 @@ const About = () => {
 
             <img src="https://fikcup.github.io/react-portfolio/assets/profile-picture.jpg" alt="Rhys Wright" id="profileImg"/>
 
-            <p>Rhys Wright is a software developer focused on improving and gaining additional experience in the tech industry. They primarily develop fullstack web applications using a variety of programming languages, frameworks, and libraries.<br /><br />
+            <p>Rhys Wright is a software developer focused on improving and gaining additional experience in the tech industry. They primarily develop backend and fullstack web applications using a variety of programming languages, frameworks, and libraries.<br /><br />
             
-            Rhys is a self taught developer who currently works as a full stack web development tutor and centralized grader for 2U. They also work as a Teaching Assistant for 2U cohorts across a variety of colleges and universities.<br /><br />
+            Rhys is a self taught developer who has previously worked as a backend developer for a startup.<br /><br />
             
-            They are currently seeking opportunities to gain additional experience working with an agile development team.</p>
+            They are currently seeking backend and fullstack job opportunities.</p>
         </section>
     );
 }
